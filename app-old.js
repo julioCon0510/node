1b6dc7296645e2365c1586aa2663d75244647c0a
julioCon0510 <44731927+julioCon0510@users.noreply.github.com>
2021-04-16 08:22:47 -0500

    node js

---
+++ app-old.js
@@ -0,0 +1,12 @@
+// const http = require("http");
+
+// http
+//   .createServer((req, res) => {
+//     res.writeHead(200, { "Content-Type": "text/plain" });
+
+//     res.write("Hola Mundo");
+//     res.end();
+//   })
+//   .listen(5000);
+
+// console.log("Escuchando en el cuerpo 5000");
---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const hbs = require("hbs");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT;
+
+// handlebar
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
+
+//servir contenido estaticos
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home", {
+    nombre: "Julio",
+    titulo: "estudiante",
+  });
+});
+
+app.get("/elements", (req, res) => {
+  res.render("elements", {
+    nombre: "Julio",
+    titulo: "estudiante",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Escuchando el puerto ${port}`);
+});
